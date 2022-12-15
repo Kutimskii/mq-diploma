8cb021c9af450b2e21de6351714798830c7e49d9
Kutimskii <111412701+Kutimskii@users.noreply.github.com>
2022-12-15 10:06:03 +0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+let popup = document.querySelector('.popup-container');
+let closePopup = document.querySelector('.popup-close');
+closePopup.onclick = function () {
+  popup.classList.add('closed')
+
+}
